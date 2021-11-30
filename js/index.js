bc01b360ba154b46643fad2fc27d637c5b14d124
lienjin <2200514176@qq.com>
2021-11-30 12:38:47 +0800

    第七周任务提交

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ $('#ebooks-banner').tyslide({
     controlsH:2,//控制按钮高度
     controlsColor:"#d7d7d7",//普通控制按钮的颜色
     controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
-});
+    });
 /*新书手风琴效果*/
     $('.ebooks .right-box ul > li').mouseenter(function(){
     //所有兄弟隐藏详情 显示标题
@@ -40,8 +40,8 @@ $('#ebooks-banner').tyslide({
     //当前:隐藏标题 显示详情
     $(this).find('.ebooks-title').hide();
     $(this).find('.desc').show();
-})
-     //图书轮播
+    })
+     /*图书轮播*/
      $('#clothes-banner').tyslide({
         boxh:286,//盒子的高度
         w:429,//盒子的宽度
@@ -54,6 +54,7 @@ $('#ebooks-banner').tyslide({
         controlsColor:"#d7d7d7",//普通控制按钮的颜色
         controlsCurrentColor:"green",//当前控制按钮的颜色
     });
+    /* */
     $('#sport-banner').tyslide({
         boxh:286,//盒子的高度
         w:429,//盒子的宽度
@@ -66,6 +67,7 @@ $('#ebooks-banner').tyslide({
         controlsColor:"#d7d7d7",//普通控制按钮的颜色
         controlsCurrentColor:"green",//当前控制按钮的颜色
     });
+    /* */
     $('#children-clothes-banner').tyslide({
         boxh:286,//盒子的高度
         w:429,//盒子的宽度
@@ -78,10 +80,7 @@ $('#ebooks-banner').tyslide({
         controlsColor:"#d7d7d7",//普通控制按钮的颜色
         controlsCurrentColor:"green",//当前控制按钮的颜色
     });
-    /*服装轮播*/
-    $('clothe .cloyhes-nav li').mouseenter(function(){
-
-    })
+    /*推广商品切换*/
     $('.promotion .promotion-title ul li').mouseenter(function () {
         //导航激活类的切换
         $(this).addClass('active').siblings().removeClass('active')
@@ -93,7 +92,56 @@ $('#ebooks-banner').tyslide({
            'left':-index * 1170
        })
     })
-    
+/*电子书table切换*/ 
+    var $lis = $('.ebooks .ebooks-nav > li');
+    $lis.mouseenter(function() {
+    // 给自己添加激活类active 把兄弟的都干掉
+	$(this).addClass('active').siblings().removeClass('active');
+    // 获取index
+	var index = $(this).index();
+    // 选中内容
+	var $ebookslist = $('ebooks-list');
+    // 显示索引等于当前index
+	$ebookslist.eq(index).show().siblings('.ebooks-list').hide();
+})
+/*服装table切换*/ 
+    var $lis = $('.clothes .clothes-nav > li');
+    $lis.mouseenter(function() {
+    // 给自己添加激活类active 把兄弟的都干掉
+	$(this).addClass('active').siblings().removeClass('active');
+    // 获取index
+	var index = $(this).index();
+    // 选中内容
+	var $clotheslist = $('clothes-list');
+    // 显示索引等于当前index
+	$clotheslist.eq(index).show().siblings('.clothes-list').hide();
+})
+/*户外运动table切换*/
+    var $lis = $('.sport .sport-nav > li');
+    $lis.mouseenter(function() {
+    // 给自己添加激活类active 把兄弟的都干掉
+	$(this).addClass('active').siblings().removeClass('active');
+    // 获取index
+	var index = $(this).index();
+    // 选中内容
+	var $sportlist = $('sport-list');
+    // 显示索引等于当前index
+	$sportlist.eq(index).show().siblings('.sport-list').hide();
+})
+    /*童装table切换*/
+    var $lis = $('.children-clothes .children-clothes-nav > li');
+    $lis.mouseenter(function() {
+    // 给自己添加激活类active 把兄弟的都干掉
+    $(this).addClass('active').siblings().removeClass('active');
+    // 获取index
+    var index = $(this).index();
+    // 选中内容
+    var $childrenlist = $('children-list');
+    // 显示索引等于当前index
+    $childrenlist.eq(index).show().siblings('.children-list').hide();
+    })
+
+    /*返回顶部*/
     $(document).scroll(function () {
         //获取距离顶部的距离
         var topDistance = $('html,body').scrollTop();
@@ -104,10 +152,42 @@ $('#ebooks-banner').tyslide({
             $('.backToTop').fadeOut();
         }
     })
-    //返回顶部功能
+    // 返回顶部功能
     $('.backToTop').click(function () {
         $('html,body').animate({
             scrollTop: 0
         },300)
        })
+    /*二维码划出*/
+    $('.qr-code .ticket').hover(function(){
+        $('.qr-code div').stop(true).animate({
+            left:'-100px'
+        })
+    },function(){
+        $('.qr-code div').stop(true).animate({
+            left:0
+        })
+    })
+    /*顶部搜索框交互*/ 
+    $(document).scroll(function(){
+        //获取到顶部的距离
+        var topDistance=$('html,body').scrollTop();
+        if(topDistance > 500){
+         $('.top-search-box').slideDown(300)
+        }else{
+         $('.top-search-box').slideUp(300)
+        }
+    })
+    /*楼层交互 */
+    $('.floor li').click(function() {
+	//获取索引
+	var index = $(this).index();
+	//选中每一个板块到顶部的偏移
+	var topOffset = $('.floorBox').eq(index).offset().top;
+	//让滚动条滚到这个位置
+	$('html,body').animate({
+		scrollTop: topOffset - 50
+	})
+})
+
     })
