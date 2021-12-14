eab4e469291839290625803fed47351365a244f5
lienjin <2200514176@qq.com>
2021-12-14 15:32:39 +0800

    第九周任务提交

---
+++ js/productDetail.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 产品详情JS文件
+ */
+$(function () {
+	//调用插件函数
+	var _magnifier = magnifier({
+		magnifier: "#magnifier1",//最外层的大容器
+		width: 340,//承载容器宽
+		height: 470,//承载容器高
+		zoom: 2//缩放比例
+	})
+
+})
